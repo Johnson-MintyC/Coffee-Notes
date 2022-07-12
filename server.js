33a27c3ff68bc4a17fb7efc4c66dcdc495b72c32
Johnson Cho <johnsoncho@Roys-MBP.gateway>
2022-07-12 20:02:21 +1000

    mongodb sessions and create working

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("express-flash");
+const mongoDBSession = require("connect-mongodb-session");
 
+const usersController = require("./controllers/users");
 const coffeelogRouter = require("./controllers/coffeelog");
 
 /////////////////////////////////////////
@@ -14,21 +18,39 @@ const coffeelogRouter = require("./controllers/coffeelog");
 /////////////////////////////////////////
 const PORT = process.env.PORT;
 const dbURL = process.env.MONGODB_URL;
+const MongoDBStore = mongoDBSession(session);
+const sessionStore = new MongoDBStore({
+  uri: dbURL,
+  collection: "sessions",
+});
 
 const app = express();
-app.use(methodOverride("_method"));
-app.use(express.static("public"));
 
 /////////////////////////////////////////
 //      Middleware
 /////////////////////////////////////////
+app.use(
+  session({
+    secret: process.env.SESSION_KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    },
+  })
+);
+app.use(flash());
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
 
 /////////////////////////////////////////
 //      Routes
 /////////////////////////////////////////
 
 app.use("/", coffeelogRouter);
+app.use("/users", usersController);
 
 //////////////////////////////////////////
 //      Connection Listeners
